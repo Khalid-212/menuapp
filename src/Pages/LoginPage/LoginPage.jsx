40f56aaceb624ba2fbde96a3c1cf36a2872758bf
khalid <khalid.ibr212@gmail.com>
2023-08-13 16:44:30 +0300

    login and signup page

--- src/Pages/LoginPage/LoginPage.jsx
+++ src/Pages/LoginPage/LoginPage.jsx
@@ -1,25 +1,44 @@
-import { React, useState } from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
-import { login, selectUser } from "../../UserSlice";
+import { login } from "../../UserSlice";
+import { createClient } from "@supabase/supabase-js"; // Import the Supabase client library
 import "./LoginPage.css";
+import { getRestaurant } from "../../supabase";
+import logo from "../../assets/../Assets/logo.svg"
+
+const supabase = createClient(
+  process.env.REACT_APP_SUPABASE_URL,
+  process.env.REACT_APP_SUPABASE_API_KEY
+);
 
 function LoginPage() {
   const navigate = useNavigate();
-  const dispatch = useDispatch();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
+  const dispatch = useDispatch();
+
+  const onSubmit = async (data) => {
+    await getRestaurant(data.username, data.password).then((res) => {
+      if (res) {
+        dispatch(login(res.restaurant_id));
+        navigate("/admin");
+      } else {
+        alert("Invalid username or password");
+      }
+    });
+  };
+  errors && console.log(errors);
 
   return (
     <div className="LoginPage">
       <form className="form" onSubmit={handleSubmit(onSubmit)}>
-        <h1>Login</h1>
+        <img className="login_logo" src={logo} alt="" />
+        <h1>Menu Plus Login</h1>
         <input
           className="formInput"
           type="text"
@@ -30,7 +49,7 @@ function LoginPage() {
           className="formInput"
           type="password"
           placeholder="password"
-          {...register("password", {})}
+          {...register("password", { required: true })}
         />
 
         <input className="btnSubmit" type="submit" />
